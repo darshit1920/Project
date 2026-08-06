74eec12a5652c80c8b89f53382a60b63e28d059b
DARSHIT AGARWAL <darshitagarwal@DARSHITs-MacBook-Air.local>
2026-08-06 15:58:37 +0530

    Resolved CORS Errors

--- backend/src/main/java/com/acme/salary/controller/EmployeeController.java
+++ backend/src/main/java/com/acme/salary/controller/EmployeeController.java
@@ -8,8 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/employees")
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/employees")   
+@CrossOrigin(origins = {"*", "http://localhost:4200"})
 public class EmployeeController {
 
     private final EmployeeService employeeService;
